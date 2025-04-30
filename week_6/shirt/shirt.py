049e004955893a98a92d9ff03b8580a46e8b7121
maitreverge_mbp <verge.f@gmail.com>
2025-04-30 17:45:49 +0200

    Finished `shirt` exercice

--- week_6/shirt/shirt.py
+++ week_6/shirt/shirt.py
@@ -1,7 +1,5 @@
 import sys
-import csv
 import os
-import PIL
 from PIL import Image, ImageOps
 
 
@@ -14,17 +12,18 @@ def print_usage(message):
 def parse_args(args):
     if len(sys.argv) != 3:
         print_usage("Incorrect number of arguments")
-    
+
     valid_extension = ("jpeg", "jpg", "png")
 
     input_file = str(args[1])
     output_file = str(args[2])
 
     # Extract what's after the last found dot.
-    ext_input = input_file[input_file.rfind(".") + 1:].lower()
-    ext_output = output_file[output_file.rfind(".") + 1:].lower()
+    # Lower the results for checking case insensitively
+    ext_input = input_file[input_file.rfind(".") + 1 :].lower()
+    ext_output = output_file[output_file.rfind(".") + 1 :].lower()
 
-    correct_extension = (ext_output in valid_extension and ext_input in valid_extension)
+    correct_extension = ext_output in valid_extension and ext_input in valid_extension
     similar_extensions = ext_input == ext_output
 
     if not correct_extension or not similar_extensions:
@@ -36,24 +35,19 @@ def parse_args(args):
 
     return input_file, output_file
 
-def get_shirt_size(shirt):
-    size = ()
-
-
-    return size
 
 def main():
     input_file, output_file = parse_args(sys.argv)
 
     with Image.open("shirt.png") as shirt:
         shirt_size = shirt.size
-        
+
         with Image.open(input_file) as img:
             # Resize and cropped image
-            ImageOps.fit(img, shirt_size)
-            img.paste(shirt)
+            img = ImageOps.fit(img, shirt_size)
+            # Third argument is a mask of the shirt image
+            img.paste(shirt, (0, 0), shirt)
             img.save(output_file)
-
 
 
 if __name__ == "__main__":
