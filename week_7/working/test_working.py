4aa133ec9fdf3ad7a5de71a27b7a7928369badfc
maitreverge_mbp <verge.f@gmail.com>
2025-05-02 18:14:56 +0200

    Hours exercice done

--- week_7/working/test_working.py
+++ week_7/working/test_working.py
@@ -1,14 +1,34 @@
 import pytest
 from working import convert
 
+
 def test_easy():
-    assert convert("9:00 AM to 9:00 PM") == "9:00 to 21:00"
-    assert convert("9:33 AM to 9:33 PM") == "9:33 to 21:33"
+    assert convert("9:00 AM to 9:00 PM") == "09:00 to 21:00"
+    assert convert("9:33 AM to 9:33 PM") == "09:33 to 21:33"
+
 
 def test_missing_minutes():
-    assert convert("9:33 AM to 9 PM") == "9:33 to 21:00"
-    assert convert("9 AM to 9:33 PM") == "9:00 to 21:33"
-    assert convert("9 AM to 9 PM") == "9:00 to 21:00"
+    assert convert("9:33 AM to 9 PM") == "09:33 to 21:00"
+    assert convert("9 AM to 9:33 PM") == "09:00 to 21:33"
+    assert convert("9 AM to 9 PM") == "09:00 to 21:00"
+
+
+def test_midday_midnight():
+    assert convert("11:59 PM to 12 AM") == "23:59 to 00:00"
+    assert convert("11:59 PM to 12:00 AM") == "23:59 to 00:00"
+    assert convert("12 AM to 12:01 AM") == "00:00 to 00:01"
+    assert convert("12:00 PM to 12:00 AM") == "12:00 to 00:00"
+    assert convert("11:59 AM to 12 PM") == "11:59 to 12:00"
+    assert convert("11:59 AM to 12:00 PM") == "11:59 to 12:00"
+    assert convert("12:00 PM to 12:01 PM") == "12:00 to 12:01"
+    assert convert("12:59 PM to 01:00 PM") == "12:59 to 13:00"
+    assert convert("12 PM to 12 AM") == "12:00 to 00:00"
+    assert convert("12:00 PM to 12:00 AM") == "12:00 to 00:00"
+
+
+def test_egde_case_mid():
+    assert convert("12 AM to 12 PM") == "00:00 to 12:00"
+
 
 def test_overflow_hours():
     with pytest.raises(ValueError):
@@ -18,7 +38,11 @@ def test_overflow_hours():
     with pytest.raises(ValueError):
         convert("21:00 AM to 9:00 PM")
 
+
 def test_incorect_data():
     with pytest.raises(ValueError):
         convert("09:00 AM to 9:00PM")
-        convert("09:00 AM to 9:00PM")
+    with pytest.raises(ValueError):
+        convert("09:00AM to 9:00 PM")
+    with pytest.raises(ValueError):
+        convert("09:00AM 9:00 PM")
--- week_7/working/working.py
+++ week_7/working/working.py
@@ -1,19 +1,42 @@
 import re
-import sys
-
-TEST = "9:55 AM to 10 PM"
 
 
 def main():
-    # print(convert(input("Hours: ").strip()))
-    print(convert(TEST))
+    print(convert(input("Hours: ").strip()))
+
+
+def append_result(pattern, hour):
+    if matches := re.search(pattern, hour):
+        hours = int(matches.group(1))
+        # Transform missing minutes into "00"
+        ext_minutes = matches.group(2)
+        minutes = "00" if ext_minutes == None else ext_minutes
+
+        # Check if need to shift +12 hours if the specified time is PM
+        is_pm = matches.group(3) == "PM"
+        # Edge case for 12PM
+        if is_pm and hours != 12:
+            hours += 12
+        # Edge case for 12AM
+        elif not is_pm and hours == 12:
+            hours -= 12
+
+        return f"{hours:02}:{minutes}"
+    else:
+        raise ValueError
 
 
 def convert(s):
-    # pattern = r'([\d]|0[1-9]|1[0-2]):() AM to 5:00 PM'
-    pattern = r'^([1-9]|0[1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM) to ([1-9]|0[1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM)$'
-    if matches := re.search(pattern, s):
-        print(matches.groups())
+    # Split the hours in two blocks, and strip both results
+    hours = [hour.strip() for hour in s.split("to")]
+    pattern = r"^([1-9]|0[1-9]|1[0-2])(?::([0-5][0-9]))? +(AM|PM)$"
+
+    result = ""
+    result += append_result(pattern, hours[0])
+    result += " to "
+    result += append_result(pattern, hours[1])
+
+    return result
 
 
 if __name__ == "__main__":
