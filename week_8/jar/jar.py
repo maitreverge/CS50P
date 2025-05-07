c5b9720de464f4897630d264109c800766f0ebdb
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-05-07 17:07:03 +0200

    Finished jar exercice

--- week_8/jar/jar.py
+++ week_8/jar/jar.py
@@ -1,6 +1,6 @@
 class Jar:
     def __init__(self, capacity=12):
-        if capacity < 0:
+        if not isinstance(capacity, int) or capacity < 0:
             raise ValueError("Capacity can't be negative")
         self._capacity = capacity
         self._size = 0
--- week_8/jar/test_jar.py
+++ week_8/jar/test_jar.py
@@ -17,6 +17,38 @@ def test_neg_input():
     with pytest.raises(ValueError):
         jar = Jar(-1)
 
-def test_neg_input():
+def test_not_str_input():
     with pytest.raises(ValueError):
         jar = Jar("hehe")
+
+def test_str():
+    jar = Jar(3)
+    assert jar.size == 0
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(2)
+    assert str(jar) == "🍪🍪🍪"
+    jar.withdraw(1)
+    assert str(jar) == "🍪🍪"
+    jar.withdraw(1)
+    assert str(jar) == "🍪"
+
+def test_deposit():
+    jar = Jar(2)
+    jar.deposit(1)
+    jar.deposit(1)
+    with pytest.raises(ValueError):
+        jar.deposit(1)
+
+def test_withdraw():
+    jar = Jar(3)
+    jar.deposit(3)
+    jar.withdraw(1)
+    jar.withdraw(1)
+    jar.withdraw(1)
+    with pytest.raises(ValueError):
+        jar.withdraw(1)
+
+
+
