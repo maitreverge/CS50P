d22fa2f794c06c432b3b4bfd90d17ca9bd6f27f4
maitreverge_mbp <verge.f@gmail.com>
2025-07-30 10:07:02 +0200

    Done last exercice

--- week_8/shirtificate/shirtificate.py
+++ week_8/shirtificate/shirtificate.py
@@ -18,15 +18,20 @@ class Shirtificate:
 
         # For the title
         pdf.set_font("Helvetica", "B", 50)
-        pdf.cell(0, 50, txt="CS50 Shirtificate", align="C", new_x="LMARGIN", new_y="NEXT")
-
+        pdf.cell(0, 50, txt="CS50 Shirtificate", align="C")
         pdf.image("shirtificate.png", x=5, y=80, w=200)
 
         # For the name
+        f_text = f"{self.name} took CS50"
+        text_width = pdf.get_string_width(f_text) / 2
         pdf.set_font("Helvetica", "B", size=30)
         pdf.set_text_color(255, 255, 255)
-        pdf.cell(0, 180, align="C", txt=f"{self.name} took CS50")
 
+        x = (210 - text_width) / 2
+        y = 180
+        pdf.text(x, y, f_text)
+
+        # Export the pdf
         pdf.output("shirtificate.pdf")
 
 
